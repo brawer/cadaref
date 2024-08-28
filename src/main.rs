1c449d9859616fa8681257f016eb72f05b9e065f
Sascha Brawer <sascha@brawer.ch>
2024-08-28 16:12:06 +0200

    Format source code with rustfmt

--- src/main.rs
+++ src/main.rs
@@ -7,21 +7,21 @@ use cadaref::Matcher;
 #[derive(Parser, Debug)]
 struct Args {
     #[arg(long, value_hint=clap::ValueHint::FilePath)]
-	points: PathBuf,
+    points: PathBuf,
 
     #[arg(long, value_hint=clap::ValueHint::FilePath)]
-	symbols: PathBuf,
+    symbols: PathBuf,
 }
 
 pub fn main() -> Result<(), Box<dyn Error>> {
-	let args = Args::parse();
-	let matcher = Matcher::new(args.points, args.symbols)?;
-	
-	let dpi = 300.0;
-	let pixels_per_meter = dpi / 2.54 * 100.0;
-	let meters_per_pixel = 500.0 / pixels_per_meter;
-	println!("Hello world {}", meters_per_pixel);
-	
-	matcher.find_matches(meters_per_pixel);
-	Ok(())
+    let args = Args::parse();
+    let matcher = Matcher::new(args.points, args.symbols)?;
+
+    let dpi = 300.0;
+    let pixels_per_meter = dpi / 2.54 * 100.0;
+    let meters_per_pixel = 500.0 / pixels_per_meter;
+    println!("Hello world {}", meters_per_pixel);
+
+    matcher.find_matches(meters_per_pixel);
+    Ok(())
 }
